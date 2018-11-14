2ed3f7d89fb390bbd659578b51beb9e4d2a58164
emsetohughes <emma.setohughes@gmail.com>
2018-11-14 18:06:06 -0500

    store user answers in an array

--- script.js
+++ script.js
@@ -5,25 +5,65 @@ const poses = {
     c: [{name: 'cow', desc: 'moo'}, {name: 'cat', desc: 'meow'}],
 }
 
+const quizApp = {};
 
-$(function(){
+quizApp.init = function(){
+    quizApp.getResults();
+};
 
-
+quizApp.getResults = function(){
     $('form').on('submit', function (event) {
-        //this is code only to be run when the form is submitted 
-        event.preventDefault();
-        //print this out to check what the event is
-        console.log(event, "ellomate");
+    //this is code only to be run when the form is submitted 
+    event.preventDefault();
+    //print this out to check what the event is
+    // console.log(event, "hola");
 
-        // Capture the values of the radio buttons
-        const sleepHours = $('input[name=sleep]:checked').val();
-        const favDrink = $('input[name=drink]:checked').val();
-        const favFruit = $('input[name=fruit]:checked').val();
-        const cryLevel = $('input[name=cry]:checked').val();
-        const socialTime = $('input[name=social]:checked').val();
+    // Capture the values of the radio buttons into an array
+    const results = [];
+    //function to push the user's answers to the array after they click submit
+    // const addResults = function () {};
+    // console.log("hellloooooo");
+    const resultsObject = $('input:checked');
+    console.log(resultsObject);
+    // console.log("byeeeeeeeee");
+    for (let i = 0; i < resultsObject.length; i++) {
+        results.push(resultsObject[i].value);
+        // console.log(resultsObject[i].value);
+    };
+    //print out array of results ie. [A,B,B,C,D]
+    console.log(results);
 
-        console.log(sleepHours, favDrink, favFruit, cryLevel, socialTime);
+    //Analyze results
+    //the analyzeResults function returns back the majority letter that the user answered
+    const letterMajority = analyzeResults(results);
 
     });
+};
+
+quizApp.analyzeResults = function(resultsArray) {
+    const aCount = 0;
+    const bCount = 0;
+    const cCount = 0;
+    const dCount = 0;
+    const eCount = 0;
+    for (let i = 0; i < resultsArray.lenth; i++) {
+        if (resultsArray[i] === "A") {
+            aCount++; 
+        } else if (resultsArray[i] === "B") {
+            bCount++;
+        } else if (resultsArray[i] === "C") {
+            cCount++;
+        } else if (resultsArray[i] === "D") {
+            dCount++;
+        } else if (resultsArray[i] === "E") {
+            eCount++;
+        }
+    }
+};
+
+$(function(){
+    quizApp.init();
+
     
 });
+
